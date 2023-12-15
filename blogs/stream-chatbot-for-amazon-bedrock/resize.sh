8a409bcfefbfbb81bc36aeca433bf4333c9006ae
Kyoungsu Park <52392004+kyopark2014@users.noreply.github.com>
2023-12-16 04:53:21 +0900

    Update resize.sh

--- blogs/stream-chatbot-for-amazon-bedrock/resize.sh
+++ blogs/stream-chatbot-for-amazon-bedrock/resize.sh
@@ -1,11 +1,13 @@
+
 #!/bin/bash
 
 # Specify the desired volume size in GiB as a command line argument. If not specified, default to 20 GiB.
 SIZE=${1:-20}
 
 # Get the ID of the environment host Amazon EC2 instance.
-INSTANCEID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/region)
+TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 60")
+INSTANCEID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/instance-id 2> /dev/null)
+REGION=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/placement/region 2> /dev/null)
 
 # Get the ID of the Amazon EBS volume associated with the instance.
 VOLUMEID=$(aws ec2 describe-instances \
@@ -27,14 +29,13 @@ while [ \
 sleep 1
 done
 
-#Check if we're on an NVMe filesystem
+# Check if we're on an NVMe filesystem
 if [[ -e "/dev/xvda" && $(readlink -f /dev/xvda) = "/dev/xvda" ]]
 then
-  # Rewrite the partition table so that the partition takes up all the space that it can.
+# Rewrite the partition table so that the partition takes up all the space that it can.
   sudo growpart /dev/xvda 1
-
-  # Expand the size of the file system.
-  # Check if we're on AL2
+# Expand the size of the file system.
+# Check if we're on AL2
   STR=$(cat /etc/os-release)
   SUB="VERSION_ID=\"2\""
   if [[ "$STR" == *"$SUB"* ]]
@@ -45,11 +46,11 @@ then
   fi
 
 else
-  # Rewrite the partition table so that the partition takes up all the space that it can.
+# Rewrite the partition table so that the partition takes up all the space that it can.
   sudo growpart /dev/nvme0n1 1
 
-  # Expand the size of the file system.
-  # Check if we're on AL2
+# Expand the size of the file system.
+# Check if we're on AL2
   STR=$(cat /etc/os-release)
   SUB="VERSION_ID=\"2\""
   if [[ "$STR" == *"$SUB"* ]]
@@ -59,3 +60,5 @@ else
     sudo resize2fs /dev/nvme0n1p1
   fi
 fi
+
+sudo xfs_growfs /dev/nvme0n1p1
